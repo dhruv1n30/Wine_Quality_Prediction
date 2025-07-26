2771d9cfe38fd4878d1ce53ffd7e7b9e38d77c41
dhruv1n30 <dhruvinvachhani25@gmail.com>
2025-07-26 18:31:09 +0530

    trials added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Wine_Quality-_Prediction
+
+1. `conda create -n wine python=3.11 -y`
+2. `conda activate wine`
+3. `pip install -r requirements.txt`
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+__version__ = '0.0.0'
+
+AUTHOR_NAME = 'Dhruvin Vachhani'
+AUTHOR_EMAIL = 'dhruvinvachhani25@gmail.com'
+
+
+setup(
+    name='wine_quality_prediction',
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[],
+)
