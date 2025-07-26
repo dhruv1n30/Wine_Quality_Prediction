6f4feafda90738e2eabca89ac6c76aa065aed94f
dhruv1n30 <dhruvinvachhani25@gmail.com>
2025-07-26 22:15:21 +0530

    data ingestion add

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Wine_Quality-_Prediction
 
+<!-- update config.yaml
+update schema.yaml
+update params.yaml
+update the entity
+update the configuration manager in src config
+update the components
+update the pipeline
+update the main.py
+update the app.py -->
+
+
+
 1. `conda create -n wine python=3.11 -y`
 2. `conda activate wine`
 3. `pip install -r requirements.txt`
--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 
 __version__ = '0.0.0'
 
 AUTHOR_NAME = 'Dhruvin Vachhani'
+SRC_REPO = "mlProject"
 AUTHOR_EMAIL = 'dhruvinvachhani25@gmail.com'
+AUTHOR_USER_NAME = 'dhruv1n30'
+REPO_NAME = 'Wine_Quality_Prediction'
 
 
-setup(
-    name='wine_quality_prediction',
+setuptools.setup(
+    name=SRC_REPO,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
     version=__version__,
-    packages=find_packages(),
-    install_requires=[],
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
 )
+
